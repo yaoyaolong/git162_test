242ef0414d8c53bf1ed521c60cf09232a9ae88dd
yaol <13001597445>
2019-09-25 22:17:46 +0800

    第一次项目

---
+++ src/main/java/com/baizhi/Git162TestApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git162TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git162TestApplication.class, args);
+    }
+
+}
